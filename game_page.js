2adad1a37ab1de3efc4ff7f6b1a6cb809eabbeb2
Aneesh-Wable <89678773+Aneesh-Wable@users.noreply.github.com>
2022-02-04 19:09:11 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,83 @@
+player1_name = localStorage.getItem("Player 1");
+  
+player2_name = localStorage.getItem("Player 2");
+
+player_1_score = 0;
+player_2_score = 0;
+
+
+
+document.getElementById("player_1_name").innerHTML = player1_name +" : ";
+
+document.getElementById("player_2_name").innerHTML = player2_name +" : ";
+
+document.getElementById("player_1_score").innerHTML= player_1_score ;
+
+document.getElementById("player_2_score").innerHTML = player_2_score ;
+
+document.getElementById("player_Question").innerHTML = "Question Turn :" + player1_name;
+
+document.getElementById("player_Answer").innerHTML = "Answer Turn :" + player2_name;
+
+function send()
+{
+    number1 = document.getElementById("number1").value ;
+    number2 = document.getElementById("number2").value ;
+    
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+    question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+    input_box = "<br>Answer: <input type='text' id ='input_check_box'>";
+    check_button = "<br><br><button class = 'btn btn-info' onclick = 'check()'>Check</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+
+}
+
+var question_turn = "player1";
+var answer_turn = "player2";
+
+function check()
+{
+    getAnswer = document.getElementById("input_check_box").value;
+    if(getAnswer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+            player_1_score = player_1_score+1;
+            document.getElementById("player_1_score").innerHTML = player_1_score;
+        }
+        else
+        {
+            player_2_score = player_2_score+1;
+            document.getElementById("player_2_score").innerHTML = player_2_score;
+        }
+
+    }
+
+    if(question_turn == "player_1")
+    {
+       question_turn = "player_2";
+       document.getElementById("player_question").innerHTML = "Question turn - "+ player2_name;
+    }
+    else
+    {
+       question_turn = "player_1";
+       document.getElementById("player_question").innerHTML = "Question turn - "+ player1_name;
+    }
+
+    if(answer_turn == "player_1")
+    {
+       answer_turn = "player_2";
+       document.getElementById("player_answer").innerHTML = "Answer turn - "+ player2_name;
+    }
+    else
+    {
+       answer_turn = "player_1";
+       document.getElementById("player_answer").innerHTML = "Answer turn - "+ player1_name;
+    }
+    document.getElementById("output").innerHTML="";
+}
